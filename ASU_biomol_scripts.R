b7d99892da5fb5b15948ed8d190dd7acc90e398e
aecahill <aecsk8gr8@yahoo.com>
2016-08-14 17:16:52 -0400

    august 14 analyses biomol
    Code for the biomol OTU analyses on the ASUs

--- ASU_biomol_scripts.R
+++ ASU_biomol_scripts.R
@@ -1,55 +1,35 @@
-asuotu<-read.table("C:/Users/acahill/Desktop/asuotu.txt",header=TRUE)
-
-samples<-c(1:33)
-otu = NULL
-
-for (i in samples) {
-  collapsed<-tapply(asuotu[,i],asuotu$class,sum)
-  otu<-cbind(otu,collapsed)
-  
-}
-
-otu
-colnames(otu)<-colnames(asuotu[1:33])
-otu2<-t(otu)
-sites<-read.table("C:/Users/acahill/Desktop/asu_biomol_sites.txt",header=TRUE)
-
-
 #need file asus3 which is the matrix of species
 #need file sites with a column of site names and region names
+#This is for biomol data, raw OTUs
 
+asus3<-read.table("C:/Users/acahill/Desktop/asu_biomol.txt",header=TRUE)
+sites<-read.table("C:/Users/acahill/Desktop/asu_biomol_sites.txt",header=TRUE)
 
 #load vegan
 library(vegan)
 
 #compute NMDS
-asusotunmds<-metaMDS(otu2)
+asus3nmds<-metaMDS(asus3)
 
 #plot NMDS in base R
-#ordiplot(asusotunmds,type="n")
-#ordiellipse(asusotunmds,groups=sites$regions$V1,draw="polygon",col="grey90",label=F)
-#orditorp(asusotunmds,display="species",col="black",air=0.01)
-#orditorp(asusotunmds,display="sites",col="red",air=0.01)
+#ordiplot(asus3nmds,type="n")
+#ordiellipse(asus3nmds,groups=sites$Sea,draw="polygon",col="grey90",label=F)
+#orditorp(asus3nmds,display="species",col="black",air=0.01)
+#orditorp(asus3nmds,display="sites",col="red",air=0.01)
 
 #analysis of similarity for sites and regions
-anosim(otu2,sites$site)
-anosim(otu2,sites$Sea)
+anosim(asus3,sites$site)
+anosim(asus3,sites$Sea)
 
 #nested anosim
-
-adonis(formula=otu2~sites$Sea+sites$site)
-
-#ordiplot(asusotunmds,type="n") #initiate plot
-#ordihull(asusotunmds,groups=sites$Sea,draw="polygon",col="grey90",label=F) #make polygons
-#orditorp(asusotunmds,display="sites",col="red",air=0.01)#add site labels
-#orditorp(asusotunmds,display="species",col="black",air=0.01) #add species labels
+adonis(formula=asus3~sites$Sea+sites$site)
 
 #moving plot to ggplot
 
-data.scores <- as.data.frame(scores(asusotunmds))
+data.scores <- as.data.frame(scores(asus3nmds))
 datascores<-cbind(data.scores,sites)
 head(datascores)
-species.scores <- as.data.frame(scores(asusotunmds, "species"))
+species.scores <- as.data.frame(scores(asus3nmds, "species"))
 species.scores$species <- rownames(species.scores)
 head(species.scores)
 
@@ -72,7 +52,7 @@ grp.g <- data.scores[datascores$Sea == "Red", ][chull(datascores[datascores$Sea 
                                                                    "Red", c("NMDS1", "NMDS2")]), ]
 
 hull.data <- rbind(grp.a, grp.b, grp.c, grp.d, grp.f, grp.g) #turn the hulls into a single dataframe
-hull.sea<-c("Adriatic","Adriatic","Adriatic","Adriatic","Baltic","Baltic","Baltic","Biscay","Biscay","Biscay","Biscay","Biscay","Black","Black","Black","Black","Gulf_of_Lions","Gulf_of_Lions","Gulf_of_Lions","Gulf_of_Lions","Red","Red","Red","Red") #add column for groups (these are based on this data only)
+hull.sea<-c("Adriatic","Adriatic","Adriatic","Adriatic","Adriatic","Baltic","Baltic","Baltic","Biscay","Biscay","Biscay","Black","Black","Black","Black","Gulf_of_Lions","Gulf_of_Lions","Gulf_of_Lions","Gulf_of_Lions","Gulf_of_Lions","Red","Red","Red","Red") #add column for groups (these are based on this data only)
 hull.data<-cbind(hull.data,hull.sea) #attach group names to hull dataframe
 
 #plot in ggplot
@@ -93,9 +73,10 @@ ggplot() +
         plot.background = element_blank())+ 
   geom_polygon(data=hull.data,aes(x=NMDS1,y=NMDS2,group=hull.sea),alpha=0.20) #add polygon based on the hulls calculated
 
+
 #diversity statistics
 
-asusdiv<-cbind(diversity(otu2,index="simpson"),sites) #calculate simpsons index, bind to site information
+asusdiv<-cbind(diversity(asus3,index="simpson"),sites) #calculate simpsons index, bind to site information
 
 colnames(asusdiv)<-c("simpsons","site","Sea") #rename columns
 
@@ -120,9 +101,11 @@ ggplot(asusdiv, aes(x=Sea, y=simpsons,color=Sea))+
   theme(axis.title.y=element_text(size=16))+
   theme(legend.position="none")+
   ylim(0,1)+
-  annotate("text", x = 1, y = 0.74, label = "b", size = 6)+
-  annotate("text", x = 2, y = 0.89, label = "b", size = 6)+
-  annotate("text", x = 3, y = 0.89, label = "b", size = 6)+
-  annotate("text", x = 4, y = 0.23, label = "a", size = 6)+
-  annotate("text", x = 5, y = 0.92, label = "b", size = 6)+
-  annotate("text", x = 6, y = 0.75, label = "b", size = 6)
+  annotate("text", x = 1, y = 0.92, label = "bc", size = 6)+
+  annotate("text", x = 2, y = 0.9, label = "ab", size = 6)+
+  annotate("text", x = 3, y = 0.95, label = "c", size = 6)+
+  annotate("text", x = 4, y = 0.57, label = "a", size = 6)+
+  annotate("text", x = 5, y = 0.99, label = "c", size = 6)+
+  annotate("text", x = 6, y = 0.87, label = "bc", size = 6)
+
+
