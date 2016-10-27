532ae8fb9d4fae07b52ee16e19cb9799aa55a997
aecahill <aecsk8gr8@yahoo.com>
2016-10-26 22:37:50 -0400

    changes to composition plot
    october 26, now have code for three plots (code is messy and really
    should be organized)

--- composition_plots.R
+++ composition_plots.R
@@ -55,7 +55,9 @@ colnames(morphofiltered)<-c("region","taxon","V3","abundance")
 
 #Graph
 
-good_palette<-c("#71742d","#b657c5","#56c267","#d64585","#68a536","#676acd","#b6b43c","#964e83","#4ca47e","#ce4e33","#5c99cf","#d18837","#cf8ecd","#be9f63","#c36261")
+colors<-brewer.pal(9,"Set1")
+pal<-colorRampPalette(colors)
+good_palette<-pal(20)
 
 compositionplot <-ggplot(morphofiltered, aes(x=region, y=abundance, fill=taxon)) +
   geom_bar(stat="identity") + 
@@ -119,15 +121,90 @@ colnames(biomolfiltered)<-c("region","taxon","V3","abundance")
 
 #Graph
 
-good_palette<-c("#71742d","#b657c5","#56c267","#d64585","#68a536","#676acd","#b6b43c","#964e83","#4ca47e","#ce4e33","#5c99cf","#d18837","#cf8ecd","#be9f63","#c36261")
+colors<-brewer.pal(9,"Set1")
+pal<-colorRampPalette(colors)
+good_palette<-pal(20)
 
-compositionbiomolplot <-ggplot(biomolfiltered, aes(x=region, y=abundance, fill=taxon)) +
+#good_palette<-c("#71742d","#b657c5","#56c267","#d64585","#68a536","#676acd","#b6b43c","#964e83","#4ca47e","#ce4e33","#5c99cf","#d18837","#cf8ecd","#be9f63","#c36261")
+
+compositioncollapsedplot <-ggplot(biomolfiltered, aes(x=region, y=abundance, fill=taxon)) +
   geom_bar(stat="identity") + 
   theme_bw()+
   theme(panel.grid.minor=element_blank(), panel.grid.major=element_blank())+
   xlab("Population")+
   theme(axis.title.x = element_text(size=12))+
   theme(axis.title.y=element_text(size=12))+
-  #scale_fill_manual(values = good_palette) +
+  scale_fill_manual(values = good_palette) +
+  theme_bw() + ylab("Read\n") + 
+  theme(axis.text.x= element_text(angle = 90))
+
+
+##biomolplots 
+
+biomol<-read.table("C:/Users/Abigail/Desktop/asu_biomol.txt",header=TRUE)
+biomolsites<-read.table("C:/Users/Abigail/Desktop/asu_biomol_sites.txt",header=TRUE)
+
+#make collapsed list of taxa across regions
+biomol<-cbind(biomol,biomolsites)
+
+seas<-c(1:1608)
+region = NULL
+
+for (i in seas) {
+  collapsed<-tapply(biomol[,i],biomol$Sea,sum)
+  region<-cbind(region,collapsed)
+  
+}
+
+#region
+colnames(region)<-colnames(biomol[1:1608])
+
+#Find taxa that represent at least 1% in the dataset
+
+tots<-colSums(region)
+grandtot<-sum(tots)
+percs<-100*(tots/grandtot)
+
+#Remove all taxa that do not represent 1% in the dataset
+
+filtered = NULL
+taxanames<-colnames(region)
+namesvec = NULL
+
+taxa<-c(1:1608)
+
+for (i in taxa) {
+  if (percs[i]>= 1) {
+    filtered<-cbind(filtered,region[,i])
+    namesvec<-c(namesvec,taxanames[i])
+  }
+}
+
+colnames(filtered)<-namesvec
+regnames<-c("Adriatic","Baltic","Biscay","Black","Gulf_of_Lions","Red")
+filtered<-as.data.frame(cbind(filtered,regnames))
+
+biomolfiltered<-gather(filtered,key=regnames,value=taxon,1:24)
+abundance<-as.numeric(biomolfiltered$taxon)
+biomolfiltered<-cbind(biomolfiltered,abundance)
+colnames(biomolfiltered)<-c("region","taxon","V3","abundance")
+
+
+#Graph
+
+colors<-brewer.pal(9,"Set1")
+pal<-colorRampPalette(colors)
+good_palette<-pal(24)
+
+#good_palette<-c("#71742d","#b657c5","#56c267","#d64585","#68a536","#676acd","#b6b43c","#964e83","#4ca47e","#ce4e33","#5c99cf","#d18837","#cf8ecd","#be9f63","#c36261")
+
+compositioncollapsedplot <-ggplot(biomolfiltered, aes(x=region, y=abundance, fill=taxon)) +
+  geom_bar(stat="identity") + 
+  theme_bw()+
+  theme(panel.grid.minor=element_blank(), panel.grid.major=element_blank())+
+  xlab("Population")+
+  theme(axis.title.x = element_text(size=12))+
+  theme(axis.title.y=element_text(size=12))+
+  scale_fill_manual(values = good_palette) +
   theme_bw() + ylab("Read\n") + 
   theme(axis.text.x= element_text(angle = 90))
