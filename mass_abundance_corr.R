04dcb2ae9edc8147187221d867014b12ffc6c7eb
aecahill <aecsk8gr8@yahoo.com>
2016-10-28 11:04:08 -0400

    correlation btwn abundance and reads
    Code for figure to correlate abundance and reads

---
+++ mass_abundance_corr.R
@@ -0,0 +1,24 @@
+##Script to make an x-y plot correlating mass and read number
+
+#read in data
+readmass<-read.table("C:/Users/acahill/Desktop/readmass.txt",header=TRUE)
+
+
+#correlate the two metrics
+cor.test(readmass$read,readmass$weight)
+
+library(ggplot2)
+
+ggplot(readmass,aes(read,weight))+
+  geom_point(aes(color=Sea), size = 4)+
+  theme_bw()+
+  theme(panel.background = element_blank(), 
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        plot.background = element_blank())+
+  scale_colour_manual(values=c("green","darkorange2","gold","black","purple","red"))+
+  theme(axis.text.x= element_text(size=16))+
+  theme(axis.text.y= element_text(size=16))+
+  theme(axis.title.x=element_text(size=16))+
+  theme(axis.title.y=element_text(size=16))+
+  xlab("\nNumber of reads")+ylab("Mass (g)\n")
